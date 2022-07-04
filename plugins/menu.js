1518e1d22d9bf00c43c1ac7656a885cdcd09039b
ramlaidi <83389072+ramlaidi@users.noreply.github.com>
2022-07-04 21:47:26 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -324,7 +324,7 @@ _${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik_
 
 Note: Jika ada Fitur yg Error Lapor ke owner
 `,
-      footer: `AhmdLui 🥶🥶`,
+      footer: `RaffiBew 🥶🥶`,
       title: judul,
       buttonText: "Klik here",
       sections
